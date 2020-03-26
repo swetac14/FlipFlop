68ae0f7e6007c08aa76372ed792ed8320ec35a0f
Sweta Chauhan <sweta.chauhan14@gmail.com>
2020-03-26 01:12:12 -0500

    Implemented memory blocks

---
+++ FlipFlop/Controller.js
@@ -0,0 +1,39 @@
+﻿
+class MemoryBlock {
+    constructor(id, frontImage, backImage) {
+        this.id = id;
+        this.blockCSS = "block";
+        this.frontImage = frontImage;
+        this.backImage = backImage;
+        this.front = false;
+        this.back = true;
+        this.frontCSS = "block-front block-face";
+        this.backCSS = "block-back block-face";
+        this.imgCSS = "block-value";
+
+    }
+}
+
+function startTimer(duration, display) {
+    var timer = 60 * duration, minutes, seconds;
+    countdown = setInterval(() => {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = `Time ${minutes}:${seconds}`;
+        if (--timer < 0) {
+            gameOver();
+        }
+    }, 1000);
+}
+
+class gameInfo {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        //  this.timer = document.getElementById('time-remaining');
+        this.flips = 0;
+    }
+}
